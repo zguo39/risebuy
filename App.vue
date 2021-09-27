595be2a73b83ed128193be4ea75a6c066ba9b269
zguo39 <zguo39@student.monash.edu>
2021-09-28 01:16:27 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,98 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			const updateManager = uni.getUpdateManager();
+			updateManager.onCheckForUpdate(function(res) {
+				// 请求完新版本信息的回调
+				if (res.hasUpdate) {
+					updateManager.onUpdateReady(function(res2) {
+						uni.showModal({
+							title: '更新提示',
+							content: '发现新版本，是否重启应用?',
+							cancelColor: '#eeeeee',
+							confirmColor: '#FF0000',
+							success(res2) {
+								if (res2.confirm) {
+									// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+									updateManager.applyUpdate();
+								}
+							}
+						});
+					});
+				}
+			});
+
+			updateManager.onUpdateFailed(function(res) {
+				// 新的版本下载失败
+				uni.showModal({
+					title: '提示',
+					content: '检查到有新版本，但下载失败，请检查网络设置',
+					success(res) {
+						if (res.confirm) {
+							// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+							updateManager.applyUpdate();
+						}
+					}
+				});
+			});
+		},
+		onShow: function() {
+			// this.getUserInfo()
+		},
+		onHide: function() {
+			
+		},
+		data() {
+			return {
+				userProfile: "",
+				nickName: "点击登录",
+				headphoto: "",
+				token: null
+			}
+		},
+		methods: {
+			getUserInfo() {
+				uni.getUserProfile({
+					desc: 'Wexin',
+					success: res => {
+						this.userProfile = JSON.parse(res.rawData)
+						this.nickName = this.userProfile.nickName
+						this.headphoto = this.userProfile.avatarUrl
+						console.log(this.nickName);
+						let name = this.nickName
+						uni.login({
+							provider: 'weixin',
+							success: async (loginRes) => {
+								// console.log(loginRes);
+								let result = await this.$httpRequest({
+									url: '/user/wxLogin',
+									data: {
+										code: loginRes.code,
+										name,
+									}
+								})
+								this.token = result.token
+								uni.setStorageSync('token', result.token)
+								uni.setStorageSync('priority', result.priority)
+								uni.setStorageSync('nickName', this.nickName)
+								uni.setStorageSync('url', this.headphoto)
+
+							}
+						});
+					}
+				})
+			},
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	uni-page-body,
+	html,
+	body {
+		height: 100%;
+		background-color: #F6F6F6;
+	}
+</style>
---
+++ main.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import App from './App'
+import ourLoading from '@/components/our-loading/our-loading.vue'
+import {
+	httpRequest
+} from './network/api.js'
+import share from '@/utils/share.js'
+
+function convertStamp(str) {
+	let newDate = new Date();
+	newDate.setTime(str);
+	return newDate.toLocaleDateString() + " " + newDate.toLocaleTimeString();
+}
+
+function beautify_time(timestamp){
+	    var mistiming = Math.round(new Date() / 1000)-timestamp;
+	    var postfix = mistiming>0 ? '前' : '后'
+	    mistiming = Math.abs(mistiming)
+	    var arrr = ['年','个月','星期','天','小时','分钟','秒'];
+	    var arrn = [31536000,2592000,604800,86400,3600,60,1];
+	 
+	    for(var i=0; i<7; i++){
+	        var inm = Math.floor(mistiming/arrn[i])
+	        if(inm!=0){
+	            return inm+arrr[i] + postfix
+	        }
+	    }
+	}
+
+Vue.component('ourLoading', ourLoading)
+Vue.prototype.$httpRequest = httpRequest
+Vue.prototype.$convertStamp = convertStamp
+Vue.prototype.$beautify_time = beautify_time
+Vue.mixin(share)
+
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+	...App
+})
+app.$mount()
